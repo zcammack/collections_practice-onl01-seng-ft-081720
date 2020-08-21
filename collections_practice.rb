aa0afd944be517e4bab679ee2a141f428f7a557e
Zackary Cammack <zack.cammack@gmail.com>
2020-08-21 13:36:06 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -44,7 +44,10 @@ def sum_array(integers)
 end
 
 def add_s(array)
-  [0, 2, 3].each_with_index.collect do |element, index|
-    element << "s"
+  [array].each_with_index.collect do |element, index|
+    if index != 1
+      element = element + "s"
+    else
+      element = element
   end
 end
